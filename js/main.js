1868bdfce54e4a8ea20c3b269667f19d46446c90
unknown <html@2-01.itfactory.neta>
2015-09-21 17:55:29 +0200

    project beállítása

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// elemek kiválasztása
+// @param selector: (string) az elemet kiválasztó szelektor.
+
+function _q( selector ){
+    
+    //elemek kiválasztása
+    var elements = document.querySelectorAll( selector );
+    return elements;
+};
